22140f1f2a770b97ef33c9eb4a3be59e184915fd
xiaojiecong <grp.wze@corp.netease.com>
2019-11-06 09:30:20 +0800

    变量名修改

--- index.js
+++ index.js
@@ -3,26 +3,26 @@ const path = require('path');
 const glob = require('glob');
 const loaderUtils = require('loader-utils');
 
-module.exports = function (content) {
-    const options = loaderUtils.getOptions(this) || {}; 
-    if(options.noCache)this.cacheable(false);
-    
+module.exports = function(content) {
+    const options = loaderUtils.getOptions(this) || {};
+    if (options.noCache) this.cacheable(false);
+
     let fileReg = /__getPath\(([^\)]+)\)/gim;
     //自定义文件 context|| 从webpack 4开始，原先的this.options.context被改进为this.rootContext
     let rootPath = options.context || this.rootContext || (this.options && this.options.context);
     let srcPath = path.join(rootPath, "/src");
     let filepath = this.context; //被处理的文件所在路径
     content = content.replace(fileReg, (ret, src) => {
-        let pathName = src.replace(/'|"/g, "");
-        let resList = glob.sync(path.join(srcPath, pathName) + "/*");
+        let folderName = src.replace(/'|"/g, "");
+        let resList = glob.sync(path.join(srcPath, folderName) + "/*");
         let result = '[';
         for (let i = 0; i < resList.length; i++) {
             let respath = path.relative(filepath, resList[i]).replace(/\\/g, "/")
             result += "require('" + respath + "')" + ","
         }
-        result = result.substr(0,result.length-1) + "]";
+        result = result.substr(0, result.length - 1) + "]";
         return result;
-       
+
     })
     return content;
 }
